af30876b36abc6cbb45800c2e22c9d546330a7a2
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-02-07 21:23:26 +0100

    02

---
+++ lib/second.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Second extends StatelessWidget {
+  const Second({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ISIMG",
+          style: TextStyle(
+            fontSize: 33,
+            color: Colors.amber[900],
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
